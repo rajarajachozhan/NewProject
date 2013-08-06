e97ab04ccc9602a1df3654de444f131059278ddc
rajarajachozhan <rajarajachozhan.natarajan@cognizant.com>
2013-08-06 11:06:46 +0100

    RN commit added groovy test

---
+++ admin/src/com/o2/test/TestGroovy.groovy
@@ -0,0 +1,12 @@
+package com.o2.test
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 06/08/13
+ * Time: 10:58
+ * To change this template use File | Settings | File Templates.
+ */
+class TestGroovy {
+
+}
